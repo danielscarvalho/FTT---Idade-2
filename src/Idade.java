858c62abe5b5a9733c88d4e1d36789a5d5210138
pro14533 <pro14533@HPFS-CE-LAB6941.cefsa.corp.local>
2018-08-21 22:20:09 -0300

    First version...

---
+++ src/Idade.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Idade {
+
+	//ఐ ఫిక్స్డ్ ఠిస్ మిస్సై ప్రోగ్రాం
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Dica: sout + Crtl + " "
+		System.out.println("Idade...");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe seu ano de nascimento: ");
+		
+		try {
+		
+			System.out.println("Sua idade é: " + (2018 - scanner.nextInt()));
+		
+		} catch(Exception e) {
+			System.err.println("Ops!!");
+			e.printStackTrace();
+			//System.out.println(e.getLocalizedMessage());
+		} //try
+		
+		scanner.close();
+	}
+
+}
